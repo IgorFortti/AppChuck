96036cc59bfc0e98601d61814491070d34d113f8
Igor Fortti <igormfortti@gmail.com>
2023-02-28 12:21:18 -0300

    Initial Commit

---
+++ AppChuck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppChuck
+//
+//  Created by Igor Fortti on 28/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
